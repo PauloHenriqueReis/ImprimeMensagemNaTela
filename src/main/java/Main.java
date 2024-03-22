44b72549711832dcd9d566357b79198804f59368
paulohenriqued3 <32979850-paulohenriqued3@users.noreply.replit.com>
2024-03-22 00:05:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+//Programa imprime uma Mensagem na Tela
+
+
+public class Main {
+  public static void main(String[] args) {
+   // System.out.println("Hello world!");
+  
+
+  // vaiaveis do tipo boleano
+
+  bolean media = true;
+  bolean frequencia = false;
+  bolean aprovado = media && frequencia;
+
+  System.out.print("Aprovação é: "+aprovado);
+System.out.print("Aprovação é :"+aprovado);
+  }
+}
